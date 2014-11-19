14a3596d25aece5f90407a8e2e525ff3c0c51392
Romualdo Dasig <dasig.rg@a5project.com>
2014-11-20 06:27:24 +0800

    Break lines

--- app/tests/controllers/UserControllerTest.php
+++ app/tests/controllers/UserControllerTest.php
@@ -1,65 +1,75 @@
 <?php
 
-class UserControllerTest extends TestCase {
+class UserControllerTest extends TestCase
+{
 
-	/**
-	 * Test index route.
-	 *
-	 * @return void
-	 */
-	public function testIndex()
+    /**
+     * Test index route.
+     *
+     * @return void
+     */
+    public function testIndex()
     {
         User::shouldReceive('all')->once()->andReturn('foo');
+
         $this->call('GET', 'v1/users');
+
         $this->assertRequestOk();
     }
 
     /**
-	 * Test store route.
-	 *
-	 * @return void
-	 */
+     * Test store route.
+     *
+     * @return void
+     */
     public function testStore()
     {
         User::shouldReceive('save')->once()->andReturn('foo');
+
         $this->call('POST', 'v1/users');
+
         $this->assertRequestOk();
     }
 
     /**
-	 * Test show route.
-	 *
-	 * @return void
-	 */
+     * Test show route.
+     *
+     * @return void
+     */
     public function testShow()
     {
         User::shouldReceive('find')->once()->andReturn('foo');
+
         $this->call('GET', 'v1/users/1');
+
         $this->assertRequestOk();
     }
 
     /**
-	 * Test update route.
-	 *
-	 * @return void
-	 */
+     * Test update route.
+     *
+     * @return void
+     */
     public function testUpdate()
     {
         User::shouldReceive('update')->once()->andReturn('foo');
+
         $this->call('PUT', 'v1/users/1');
+
         $this->assertRequestOk();
     }
 
     /**
-	 * Test destroy route.
-	 *
-	 * @return void
-	 */
+     * Test destroy route.
+     *
+     * @return void
+     */
     public function testDestroy()
     {
         User::shouldReceive('delete')->once()->andReturn('foo');
+
         $this->call('DELETE', 'v1/users/1');
+
         $this->assertRequestOk();
     }
-
 }
